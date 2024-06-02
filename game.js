f552ce2b85c575272df08052d254c40542a9e2a6
Jonathan Soo <soojonathan07@gmail.com>
2024-06-02 04:06:17 -0700

    Changing the style and look of the webpage

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var t = [];
 const div = document.createElement("div");
+div.setAttribute("id", "word")
 fetch("words.txt")
     .then(function (res) {
         return res.text();
@@ -8,6 +9,7 @@ fetch("words.txt")
         let text = data.toString().split("\n");
         for (let i = 0; i < text.length; i++) {
             let node2 = document.createTextNode(text[i] + " ");
+            // node2.setAttribute("id", "word");
             div.appendChild(node2);
             t.push(text[i]);
         }
