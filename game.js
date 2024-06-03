109c0e1a5eeb267831abdf91cf1dc310b5e42f84
Jonathan Soo <soojonathan07@gmail.com>
2024-06-03 00:47:40 -0700

    finished the general style of the webpage:

--- game.js
+++ game.js
@@ -7,14 +7,13 @@ fetch("words.txt")
     })
     .then(function (data) {
         let text = data.toString().split("\n");
-        for (let i = 0; i < text.length; i++) {
+        for (let i = 0; i < 25; i++) {
             let node2 = document.createTextNode(text[i] + " ");
             // node2.setAttribute("id", "word");
             div.appendChild(node2);
             t.push(text[i]);
         }
     });
-console.log(t);
 const element = document.getElementById("maintext");
 element.appendChild(div);
 
