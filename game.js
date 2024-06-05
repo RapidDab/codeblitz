c80993b024aec2f35af563c7b568a94244f8bae3
Jonathan Soo <soojonathan07@gmail.com>
2024-06-05 00:30:02 -0700

    Oranized the .js file in classes as well as accounted for different windows sizes and resizing

--- game.js
+++ game.js
@@ -1,20 +1,46 @@
-var t = [];
-const div = document.createElement("div");
-div.setAttribute("id", "word")
-fetch("words.txt")
-    .then(function (res) {
-        return res.text();
-    })
-    .then(function (data) {
-        let text = data.toString().split("\n");
-        for (let i = 0; i < 25; i++) {
-            let node2 = document.createTextNode(text[i] + " ");
-            // node2.setAttribute("id", "word");
-            div.appendChild(node2);
-            t.push(text[i]);
+class Game {
+    constructor() {
+        this.text = document.getElementById("maintext");
+        this.window = window.getComputedStyle(this.text);
+        this.language = document.getElementById("lang");
+        this.typer = document.getElementById("typer");
+    }
+    createMainText() {
+        var t = [];
+        const div = document.createElement("div");
+        div.setAttribute("id", "word");
+        fetch("words.txt")
+            .then(function (res) {
+                return res.text();
+            })
+            .then(function (data) {
+                let text = data.toString().split("\n");
+                for (let i = 0; i < text.length; i++) {
+                    let node2 = document.createTextNode(text[i] + " ");
+                    div.appendChild(node2);
+                    t.push(text[i]);
+                }
+            });
+        this.text.appendChild(div);
+    }
+    setUpAssets () {
+        function resize() {
+            game.language.style.top = `${game.window.top.split('px')[0]-150}px`
+            game.typer.style.left = `${game.window.left.split('px')[0]-500}px`
+            game.typer.style.top = `${game.window.top.split('px')[0]-100}px`
         }
-    });
-const element = document.getElementById("maintext");
-element.appendChild(div);
+        resize()
+        window.addEventListener("resize", resize)
+    }
+}
+
+const game = new Game();
+
+game.createMainText();
+game.setUpAssets();
+
+
+
+
 
 
